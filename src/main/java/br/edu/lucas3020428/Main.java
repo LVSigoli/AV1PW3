75627a6a68b473db944707254b44f0935ed3ac71
LVSigoli <lucasviniciussigoli@gmail.com>
2025-02-25 18:26:41 -0300

    feat: Implement medu and Aluno data class

--- src/main/java/br/edu/lucas3020428/Main.java
+++ src/main/java/br/edu/lucas3020428/Main.java
@@ -1,17 +1,63 @@
 package br.edu.lucas3020428;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                Scanner scanner = new Scanner(System.in);
+                int option;
+
+                do {
+                    System.out.println("** CADASTRO DE ALUNOS **");
+
+                    System.out.println("1 - Cadastrar aluno");
+                    System.out.println("2 - Excluir aluno");
+                    System.out.println("3 - Alterar aluno");
+                    System.out.println("4 - Buscar aluno pelo nome");
+                    System.out.println("5 - Listar alunos (com status aprovação)");
+                    System.out.println("6 - FIM");
+
+                    System.out.print("Escolha uma opção: ");
+                    option = scanner.nextInt();
+
+                    switch (option) {
+                        case 1:
+//                            cadastrarAluno( scanner);
+                            break;
+                        case 2:
+//                            excluirAluno( scanner);
+                            break;
+                        case 3:
+//                            alterarAluno( scanner);
+                            break;
+                        case 4:
+//                            buscarAluno( scanner);
+                            break;
+                        case 5:
+//                            listarAlunos(em);
+                            break;
+                        case 6:
+                            System.out.println("Saindo...");
+                            break;
+                        default:
+                            System.out.println("Opção inválida!");
+                    }
+                } while (option != 6);
+            }
+
+            private static void cadastrarAluno() {}
+
+            private static void excluirAluno() {}
+
+            private static void alterarAluno() {}
+
+            private static void buscarAluno() {}
+
+            private static void listarAlunos() {}
+
+
         }
-    }
-}
+
--- src/main/java/br/edu/lucas3020428/aluno/Aluno.java
+++ src/main/java/br/edu/lucas3020428/aluno/Aluno.java
@@ -1,4 +1,20 @@
 package br.edu.lucas3020428.aluno;
 
+// External Libraries
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="alunos")
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String ra;
+    private String email;
+    private BigDecimal nota1;
+    private BigDecimal nota2;
+    private BigDecimal nota3;
 }
