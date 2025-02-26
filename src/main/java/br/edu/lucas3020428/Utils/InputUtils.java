5a0a88e21ecc045b4e55824605a039a64a60a076
LVSigoli <lucasviniciussigoli@gmail.com>
2025-02-25 22:23:39 -0300

    feat: Adds validation to number inputs stay between 0 - 10

--- src/main/java/br/edu/lucas3020428/Utils/InputUtils.java
+++ src/main/java/br/edu/lucas3020428/Utils/InputUtils.java
@@ -11,8 +11,14 @@ public class InputUtils {
             try {
                 System.out.print(mensagem);
                 String entrada = scanner.next().replace(",", ".");
+                BigDecimal valor = new BigDecimal(entrada);
 
-                return new BigDecimal(entrada);
+
+                if (valor.compareTo(BigDecimal.ZERO) >= 0 && valor.compareTo(BigDecimal.TEN) <= 0) {
+                    return valor;
+                } else {
+                    System.out.println("Erro: Digite um número entre 0 e 10!");
+                }
 
             } catch (NumberFormatException e) {
                 System.out.println("Erro: Digite um número válido!");
